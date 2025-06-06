9a4aca5e64969433f7f51a7c4a923dc3d3274d47
sonja <signtieva@gmail.com>
2025-06-06 15:51:50 +0300

    add client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
 
-export default function Home() {
+export default async function Home() {
+  const headersList = await headers();
+  const userAgent = headersList.get('user-agent');
+
+  console.log('User-Agent:', userAgent);
+
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
     </main>
   );
 }
