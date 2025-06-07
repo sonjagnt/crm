159b728507a894185bc7a94112103b374e43646c
sonja <signtieva@gmail.com>
2025-06-07 12:06:14 +0300

    add layout

--- src/app/(admin)/companies/[...id]/page.tsx
+++ src/app/(admin)/companies/[...id]/page.tsx
@@ -9,7 +9,6 @@ export default async function Page({ params }: PageProps) {
   return (
     <>
       <Header>Companies ({resolvedParams.id})</Header>
-      <p>{new Date().toTimeString()}</p>
     </>
   );
 }
